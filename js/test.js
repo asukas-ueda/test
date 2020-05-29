d2562198b51f1eef6e64faf3f887aef68f3a77de
上田あすか <asukas.ueda@gmail.com>
2020-05-29 18:21:23 +0900

    CSSプルダウンメニューの実装。

--- js/test.js
+++ js/test.js
@@ -1,14 +1,3 @@
-$(function() {
-    var nav = $('.nav');
-    $('li', nav)
-    .mouseover(function(e) {
-    $('ul', this).stop().slideDown('fast');
-    })
-    .mouseout(function(e) {
-    $('ul', this).stop().slideUp('fast');
-    });
-    });
-
 $(function(){
     var setImg = '#viewer';
     var fadeSpeed = 1000;
